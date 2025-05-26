9d45f3d33ddf864b955fbed11985495fdd1a8662
ahad <syedabdulahad@innova360.io>
2025-05-27 01:16:01 +0500

    Features Made

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
 import Navbar from "../components/Navbar"
 import Hero from "../sections/Hero"
-// import Features from "../sections/Features"
+import Features from "../sections/Features"
 // import ConstructionPages from "../sections/ConstructionPages"
 // import OtherPages from "../sections/OtherPages"
 // import Footer from "../components/Footer"
@@ -10,8 +10,8 @@ export default function Home() {
     <div className="bg-[var(--black)] text-white">
       <Navbar />
       <Hero />
-      {/* <Features />
-      <ConstructionPages />
+      <Features />
+      {/* <ConstructionPages />
       <OtherPages />
       <Footer /> */}
     </div>
