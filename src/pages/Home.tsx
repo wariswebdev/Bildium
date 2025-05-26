32751e8d04c85aab4f27af5d8dc18c90e8c93b9c
ahad <syedabdulahad@innova360.io>
2025-05-27 00:27:30 +0500

    Navbar made

---
+++ src/pages/Home.tsx
@@ -0,0 +1,19 @@
+import Navbar from "../components/Navbar"
+// import Hero from "../sections/Hero"
+// import Features from "../sections/Features"
+// import ConstructionPages from "../sections/ConstructionPages"
+// import OtherPages from "../sections/OtherPages"
+// import Footer from "../components/Footer"
+
+export default function Home() {
+  return (
+    <div className="bg-[var(--black)] text-white">
+      <Navbar />
+      {/* <Hero />
+      <Features />
+      <ConstructionPages />
+      <OtherPages />
+      <Footer /> */}
+    </div>
+  )
+}
